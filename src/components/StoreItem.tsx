f0cc100036271b502b306baee80d174cf44a15ac
pavelFaraday <devstudio.corp@gmail.com>
2023-04-01 21:10:07 +0400

    Create Cart buttons in Cards

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 interface StoreItemProps {
@@ -9,8 +9,9 @@ interface StoreItemProps {
 }
 
 function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+	const quantity = 1;
 	return (
-		<Card>
+		<Card className="h-100">
 			<Card.Img
 				variant="top"
 				src={imgUrl}
@@ -24,6 +25,32 @@ function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 						{formatCurrency(price)}
 					</span>
 				</Card.Title>
+				<div className="mt-auto">
+					{quantity === 0 ? (
+						<Button className="w-100">+ Add To Cart</Button>
+					) : (
+						<div
+							className="d-flex align-items-center flex-column"
+							style={{ gap: ".5rem" }}
+						>
+							<div
+								className="d-flex align-items-center justify-content-center"
+								style={{ gap: ".5rem" }}
+							>
+								<Button>-</Button>
+								<div>
+									<span className="fs-3">
+										{quantity} in Cart
+									</span>
+								</div>
+								<Button>+</Button>
+							</div>
+							<Button variant="danger" size="sm">
+								Remove
+							</Button>
+						</div>
+					)}
+				</div>
 			</Card.Body>
 		</Card>
 	);
