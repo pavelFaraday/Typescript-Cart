fb351075b8381072b997dcaee47d521c031928fa
pavelFaraday <devstudio.corp@gmail.com>
2023-04-02 14:46:20 +0400

    ShoppingCart Offcanvas

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { useContext, createContext, ReactNode, useState } from "react";
+import ShoppingCart from "../components/ShoppingCart";
 
 interface ShoppingCartProviderProps {
 	children: ReactNode;
@@ -92,6 +93,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 			}}
 		>
 			{children}
+			<ShoppingCart isOpen={isOpen} />
 		</ShoppingCartContext.Provider>
 	);
 }
